176a4995b5b64d2959dbbcd960924147d6230646
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-07-11 15:09:23 +0800

    Update solving-equations.h

--- solving-equations.h
+++ solving-equations.h
@@ -1,26 +1,6 @@
 #ifndef SOLVING_EQUATIONS_SOLVING_EQUATIONS_H
 #define SOLVING_EQUATIONS_SOLVING_EQUATIONS_H
 
-#include <iostream>
-#include <fstream>
-#include <filesystem>
-#include <vector>
-#include <cmath>
-
-using std::cout;
-using std::endl;
-using std::cin;
-using std::vector;
-using std::fstream;
-using std::ios;
-using std::setprecision;
-
-//迭代次数限制
-#define time_limit 100000
-
-//方程式
-struct equations;
-
 //总方程读取
 class all {
 public:
@@ -36,19 +16,19 @@ public:
 //二分法
 class HalfDivide : public all {
 public:
-    void main() {};
+    void main() ;
 };
 
 //牛顿迭代法
 class Newton : public all {
 public:
-    void main() {};
+    void main() ;
 };
 
 //弦截法
 class Secant_method : public all {
 public:
-    void main() {};
+    void main() ;
 };
 
 #endif //SOLVING_EQUATIONS_SOLVING_EQUATIONS_H
