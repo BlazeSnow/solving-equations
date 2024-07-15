4ee00c8b8b439990f53680b55f2785706aabbd08
BlazeSnow <129476965+BlazeSnow@users.noreply.github.com>
2024-07-15 20:57:59 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -224,7 +224,7 @@ int main() {
 	system("chcp 65001");
 	cout << "Copyright (C) 2024 BlazeSnow.保留所有权利。" << endl;
 	cout << "本程序以GNU General Public License v3.0的条款发布。" << endl;
-	cout << "当前程序版本号：v1.0.1" << endl;
+	cout << "当前程序版本号：v1.0.2" << endl;
 	cout << "https://github.com/BlazeSnow/solving-equations" << endl << endl;
 	fstream file("solving-equations.txt", ios::in);
 	if (file.is_open()) {
